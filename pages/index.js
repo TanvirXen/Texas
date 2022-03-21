9896998223d0e092959c0d7858b5685f967da8bf
TanvirXen <mail@tanvirishtiaq.com>
2022-03-21 21:24:30 +0600

    retry

--- pages/index.js
+++ pages/index.js
@@ -555,19 +555,4 @@ export default function Home() {
 		</div>
 	);
 }
-export async function getStaticPaths() {
-	const sitemap = require("nextjs-sitemap-generator");
 
-	const fs = require("fs");
-
-	const BUILD_ID = fs.readFileSync(".next/BUILD_ID").toString();
-	sitemap({
-		baseUrl: "https://www.texsasbd.com/",
-		// If you are using Vercel platform to deploy change the route to /.next/serverless/pages
-		pagesDirectory:
-			__dirname + "/.next/serverless/pages " + BUILD_ID + "/pages",
-		targetDirectory: "public/",
-		ignoredExtensions: ["js", "map"],
-		ignoredPaths: ["assets"], // Exclude everything that isn't static page
-	});
-}
